d48b32e0dfebb6f30cad8e38fff3b462bdc21cad
falakfatma <102820083+falakfatma@users.noreply.github.com>
2023-07-16 07:06:46 +0000

    Edit Option and Lower/Upper Case
    Now You Can Edit Text

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,29 @@ import Navbar from '/./Components/Navbar'
 export default function App() {
   const [text, setText] = useState("enter text");
   function onchangeFun(e) {
-    console.log("on change")
-    console.log(e.target.value)
-    // console.log(e)
+   setText(e.target.value) 
   }
-  function CapitalizeForm ()
-  {
-    // let capitalize  = text.c
-    // setText("")
+  function UpperCaseForm (){
+    let uppercase  = text.toUpperCase()
+    setText({uppercase}.uppercase)
+    console.log(text,{uppercase})
   } 
-  function UpperCaseForm ()
-  {
-    // let capitalize  = text.c
-    // setText("")
-  } 
+  function LowerCaseForm (){
+    let lowerCase = text.toLowerCase()
+    setText(lowerCase)
+  }
+  function CapitalizeCaseForm(){
+    
+  }
   return (
     <main>
       <Navbar title="TextUtils2" />
       <div className="mt-3 container">
         <textarea className="form-control" id="exampleFormControlTextarea1" value={text} onChange={onchangeFun} rows="7"></textarea>
-        <button className="mt-2 btn btn-primary" onClick={CapitalizeForm()}> Click for Capitalize Text</button>
-        <button className="mt-2 mx-3 btn btn-primary" onClick={UpperCaseForm()}> Click for Upper Case Text</button>
+        {/* <button className="mt-2 btn btn-primary" onClick={CapitalizeForm()}> Click for Capitalize Text</button> */}
+        <button className="mt-2 mx-3 btn btn-primary" onClick={UpperCaseForm}> Convert To Upper Case</button>
+        <button className="mt-2 mx-3 btn btn-primary" onClick={LowerCaseForm}> Convert To Lower Case</button>
+        <button className="mt-2 mx-3 btn btn-primary" onClick={CapitalizeCaseForm}> Convert To Capitalize Case</button>
       </div>
     </main>
   )
